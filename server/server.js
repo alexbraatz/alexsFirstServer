78fff142d00ef90964986b27a62bcb61a0021706
alex braatz <alexjbraatz@gmail.com>
2021-03-09 15:37:21 -0600

    create first GET route and long devices array to DOM

--- server/server.js
+++ server/server.js
@@ -4,8 +4,16 @@ const express = require( 'express' );
 const app = express();
 // tell the server to use "server/public" as the root of the website
 app.use( express.static( 'server/public' ) );
+// globals
+const port = 5000;
+let devices = [ 'iphone X', 'macbookPro M1' ];
 // tell our app to spin up and listen
-app.listen( 5000, ()=>{ // one port to not use is 5432 or 6000
+app.listen( port, ()=>{ // one port to not use is 5432 or 6000
     // stick to 5000 or 3000 to be safe in Prime
     console.log( 'server is up' ); 
 });
+// our first GET route 
+app.get( '/devices', ( req, res )=>{ // remember req/res order by alphabet
+    console.log( '/dvices GET' );
+    res.send( devices )
+})
